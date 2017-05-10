97988ae70f5fd883a81aa328079084ffa4c7dd70
bruno almeida <bruno.almeida@bresimar.pt>
2017-05-10 09:16:00 +0100

    first commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MenusModelView modelView = new MenusModelView
+            {
+                Name = "MainMVVM",
+                PageViewModels = new List<MenusModelView>{
+                    new MenusModelView{
+                        Name = "Tab 1",
+                        PageViewModels = new List<MenusModelView>{
+                            new MenusModelView{
+                                Name = "Group 1",
+                                PageViewModels = new List<MenusModelView>{
+                                    new MenusModelView{
+                                        Name = "Label 1"
+                                    },
+                                    new MenusModelView{
+                                        Name = "Label 2"
+                                    },
+                                }
+                            },
+                            new MenusModelView{
+                                Name = "Group 2",
+                                PageViewModels = new List<MenusModelView>{
+                                    new MenusModelView{
+                                        Name = "Label 3"
+                                    },
+                                    new MenusModelView{
+                                        Name = "Label 4"
+                                    },
+                                }
+                            },
+                        }
+                    },
+                    new MenusModelView{
+                        Name = "Tab 2",
+                        PageViewModels = new List<MenusModelView>{
+                            new MenusModelView{
+                                Name = "Group 3",
+                                PageViewModels = new List<MenusModelView>{
+                                    new MenusModelView{
+                                        Name = "Label 5"
+                                    },
+                                }
+                            },
+                            new MenusModelView{
+                                Name = "Group 4",
+                                PageViewModels = new List<MenusModelView>{
+                                    new MenusModelView{
+                                        Name = "Label 6"
+                                    },
+                                }
+                            },
+                        }
+                    },
+                }
+            };
+
+            this.DataContext = modelView;
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MenusModelView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MenusModelView
+    {
+        public List<MenusModelView> PageViewModels { get; set; }
+        public string Name { get; set; }
+    }
+}
